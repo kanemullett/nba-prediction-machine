8ae8a0f4810b4ec3c6efc6091f4c548c624ce073
Kane Mullett <kane.mullett@savient.uk.com>
2023-12-15 22:42:31 +0000

    More appropriate name

--- python/controller/data_scraping_controller.py
+++ python/controller/data_scraping_controller.py
@@ -3,10 +3,10 @@ from model.models import EventResponse
 from service.data_scraping_service import DataScrapingService
 
 
-app = FastAPI()
+scrape = FastAPI()
 data_scraping_service = DataScrapingService()
 
 
-@app.get("/scrape/games")
+@scrape.get("/scrape/games")
 async def scrape_games(month: str, year: int) -> EventResponse:
     return data_scraping_service.scrape_games(month, year)
