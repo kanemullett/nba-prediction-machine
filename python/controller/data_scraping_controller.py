e4c6d80000a18de9e0ac93a1faaf13f54880a739
Kane Mullett <kane.mullett@savient.uk.com>
2023-12-16 14:45:14 +0000

    All sorted

--- python/controller/data_scraping_controller.py
+++ python/controller/data_scraping_controller.py
@@ -19,12 +19,23 @@ async def scrape_games(month: str, year: int) -> EventResponse:
     return data_scraping_service.scrape_games(month, year)
 
 
-@scrape.get("/scrape/stats")
-async def scrape_stats(team: str) -> EventResponse:
+@scrape.get("/scrape/stats/team")
+async def scrape_team_stats(team: str) -> EventResponse:
     """
     Scrape a team's stats for every season in the NBA's three point era (1979-80 onwards).
 
     :param team: The team's three letter abbreviation.
-    :return: Scraped stats.
+    :return: Scraped team stats.
     """
-    return data_scraping_service.scrape_stats(team)
+    return data_scraping_service.scrape_team_stats(team)
+
+
+@scrape.get("/scrape/stats/opponent")
+async def scrape_opponent_stats(team: str) -> EventResponse:
+    """
+    Scrape a team's opponent stats for every season in the NBA's three point era (1979-80 onwards).
+
+    :param team: The team's three letter abbreviation.
+    :return: Scraped opponent stats.
+    """
+    return data_scraping_service.scrape_opponent_stats(team)
